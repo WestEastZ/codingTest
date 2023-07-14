03dfa047b970f75fe6bdee80b966d3e69dc46963
West_ <tjehdwn10110@naver.com>
2023-07-14 19:23:21 +0900

    [level 1] Title: 하샤드 수, Time: 0.06 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12947. 하샤드 수/하샤드 수.js
@@ -0,0 +1,11 @@
+function solution(x) {
+  let toNum = (e) => parseInt(e);
+  let arr = x.toString().split("").map(toNum);
+  let sum = 0
+ 
+    for(let i=0; i<arr.length; i++) {
+    sum += arr[i]
+}
+   return x%sum === 0 ? true : false
+    
+}    
