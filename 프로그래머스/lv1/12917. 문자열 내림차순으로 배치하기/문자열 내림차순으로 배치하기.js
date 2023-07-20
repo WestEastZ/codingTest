acfe9ceada3e2b99946ae181ca5f41351592517e
West_ <tjehdwn10110@naver.com>
2023-07-21 01:32:04 +0900

    [level 1] Title: 문자열 내림차순으로 배치하기, Time: 0.09 ms, Memory: 33.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12917. 문자열 내림차순으로 배치하기/문자열 내림차순으로 배치하기.js
@@ -0,0 +1,17 @@
+function solution(s) {
+    let answer = s.split('');
+    let lowCaseArr = [];
+    let upCaseArr = [];
+
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] !== s[i].toUpperCase()) {
+      lowCaseArr.push(s[i]);
+    } else {
+        upCaseArr.push(s[i])
+    }
+  }
+    const newlowCaseArr = lowCaseArr.sort((a, b) => a > b ? -1 : 1).join('');
+    const newUpCaseArr = upCaseArr.sort((a, b) => a > b ? -1 : 1).join('');
+    
+    return newlowCaseArr + newUpCaseArr
+}
