498307a844c73cc7f47a64f7d004401ec710960c
West_ <tjehdwn10110@naver.com>
2023-07-23 23:08:20 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 0.16 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.js
@@ -0,0 +1,10 @@
+
+function solution(left, right) {
+    let answer = 0;
+    for(let num=left;num<=right;num++){
+        const isSqrtNumberExist = Number.isInteger(Math.sqrt(num))
+       
+        answer += (isSqrtNumberExist ? -1 : 1) * num
+    }
+    return answer
+}
