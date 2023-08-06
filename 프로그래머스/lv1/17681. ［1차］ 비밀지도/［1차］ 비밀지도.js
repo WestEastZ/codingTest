5908503c61bc8b2417dae7c28a820051920a96d3
West_ <tjehdwn10110@naver.com>
2023-08-07 00:07:46 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.17 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.js
@@ -0,0 +1,33 @@
+function solution(n, arr1, arr2) {
+  let sum = [];
+  let fir = [];
+  let sec = [];
+
+    // 5자리 2진수 변환
+  for (let i = 0; i < n; i++) {
+    let arr1Bin = arr1[i].toString(2).padStart(n, "0");
+    let arr2Bin = arr2[i].toString(2).padStart(n, "0");
+
+      // 각 배열에 push
+    fir.push(arr1Bin);
+    sec.push(arr2Bin);
+  }
+
+    // 1, 0 비교하여 str에 dlqfur
+  for (let i = 0; i < n; i++) {
+    let str = "";
+    for (let j = 0; j < n; j++) {
+      if (fir[i][j] == 1 || sec[i][j] == 1) {
+        str += "1";
+      } else {
+        str += "0";
+      }
+    }
+    let toHash = str.replaceAll(1, "#");
+    let toSpasce = toHash.replaceAll(0, " ");
+
+    sum.push(toSpasce);
+  }
+  return sum;
+}
+
