723a2908b19ff7307162ea5d7a0843b9e6e13f4f
West_ <tjehdwn10110@naver.com>
2023-08-15 23:13:45 +0900

    [level 1] Title: 행렬의 덧셈, Time: 10.72 ms, Memory: 66.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12950. 행렬의 덧셈/행렬의 덧셈.js
+++ 프로그래머스/lv1/12950. 행렬의 덧셈/행렬의 덧셈.js
@@ -1,4 +1,5 @@
 function solution(arr1, arr2) {
-  arr1.forEach((x, i) => x.forEach((y, j) => arr1[i][j] += arr2[i][j]));
-  return arr1;
+arr1.forEach((x, i, z) => x.forEach((y, j) => z[i][j] += arr2[i][j]))
+    
+return arr1
 }
