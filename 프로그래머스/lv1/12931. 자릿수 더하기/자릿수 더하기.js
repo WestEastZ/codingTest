57e1005b12d79dae3966be12f70f3dc8805ea182
West_ <tjehdwn10110@naver.com>
2023-08-17 17:13:03 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12931. 자릿수 더하기/자릿수 더하기.js
+++ 프로그래머스/lv1/12931. 자릿수 더하기/자릿수 더하기.js
@@ -1,10 +1,21 @@
-function solution(n){
-    let answer = 0;
-    n = n.toString()
-    
-    for(let i=0; i < n.length; i++) {
-        answer += parseInt(n[i]);
+function solution(n) {
+  return n
+    .toString()
+    .split("")
+    .map((v) => parseInt(v))
+    .reduce((acc, cur) => acc + cur, 0);
+
+  // .reduce((acc, cur) => acc+cur)
+
+  //     let answer = 0;
+  //     n = n.toString()
+
+  //     for(let i=0; i < n.length; i++) {
+  //         answer += parseInt(n[i]);
+  // }
+  //     return answer
 }
-    return answer
-}
+
+
+
 
