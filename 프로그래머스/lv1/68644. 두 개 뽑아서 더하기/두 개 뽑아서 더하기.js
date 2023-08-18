5d25d96c0c0255f72b4a835b653b0cee3c7e02b6
West_ <tjehdwn10110@naver.com>
2023-08-19 00:54:09 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 209.49 ms, Memory: 36.7 MB -BaekjoonHub

--- 프로그래머스/lv1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.js
+++ 프로그래머스/lv1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.js
@@ -2,15 +2,20 @@ function solution(numbers) {
   let newArray = [];
 
   for (let i = 0; i < numbers.length; i++) {
-    for (let j = i+1; j < numbers.length; j++) { 
-        newArray.push(numbers[i]+numbers[j])
-        newArray.sort((a,b) => a-b)
+    for (let j = i + 1; j < numbers.length; j++) {
+      newArray.push(numbers[i] + numbers[j]);
+      newArray.sort((a, b) => a - b);
     }
   }
-    const setArray = new Set(newArray)
-    const answer = [...setArray]
-    return answer
+  const setArray = new Set(newArray);
+  const answer = [...setArray];
+  return answer;
 }
 
-//Set을 쓰면 중복을 제거한다. 이를 이용해보자.
+// numbers.forEach((v, i) => {
+//     numbers.forEach((v2, i2) => {
+//         if (i !== i2) newArray.push(v + v2);
+//     })
+// })
 
+//Set을 쓰면 중복을 제거한다. 이를 이용해보자.
