0423506e2a220d1e071408a085687d839ffd7423
West_ <tjehdwn10110@naver.com>
2023-08-23 06:51:30 -0700

    [unrated] Title: 푸드 파이트 대회, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/134240. 푸드 파이트 대회/푸드 파이트 대회.js
+++ 프로그래머스/unrated/134240. 푸드 파이트 대회/푸드 파이트 대회.js
@@ -5,5 +5,6 @@ function solution(food) {
     answer += i.toString().repeat(parseInt(v/2)) 
 })
     
-    return answer + 0 + answer.split("").reverse().join("");
+    // return answer + 0 + answer.split("").reverse().join("");
+    return `${answer}0${answer.split("").reverse().join("")}`
 }
