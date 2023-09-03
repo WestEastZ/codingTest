31497176162b15a3f4bd14f5f04a43939e8145fd
West_ <tjehdwn10110@naver.com>
2023-09-03 23:06:48 +0900

    [unrated] Title: 명예의 전당 (1), Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/138477. 명예의 전당 （1）/명예의 전당 （1）.js
@@ -0,0 +1,16 @@
+function solution(k, score) {
+  let answer = [];
+  let arrKing = [];
+
+  score.forEach((v) => {
+      arrKing.push(v)
+      arrKing.sort((a,b) => b-a)
+      
+      if(arrKing.length > k) {
+        arrKing.pop()
+}
+      answer.push(arrKing[arrKing.length-1])
+  });
+    
+    return answer
+}
