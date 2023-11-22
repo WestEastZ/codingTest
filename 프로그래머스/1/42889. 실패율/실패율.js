294b97b79c07c7de7009bbf564365feae1cbd62a
West_ <tjehdwn10110@naver.com>
2023-11-22 18:11:27 +0900

    [level 1] Title: 실패율, Time: 0.15 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/42889. 실패율/실패율.js
+++ 프로그래머스/1/42889. 실패율/실패율.js
@@ -11,41 +11,17 @@ function solution(N, stages) {
         const tryUser = stages.filter((num) => num >= i).length
         
         // 스테이지 별 실패율
-     
         info.push(failUser/tryUser)
     }
     
+    // 실패율, 스테이지 객체 생성
     const indexInfo = info.map((value, index) => ({value, index}))
-    const indexedArr = indexInfo.sort((a,b) => b.value - a.value || a.index - b.index)
-  
     
-    let resultArr = indexedArr.map(obj => {
+    // 실패율 내림차순, 스테이지 오름차순 정렬
+    const indexedArr = indexInfo.sort((a,b) => b.value - a.value).map(obj => {
         return obj.index + 1
     })
+  
     
-    console.log(resultArr)
-    
-    return resultArr
-    
-    
-   
-    // 객체 키로 stages가 들어가고 filter로 나온 갯수를 값으로 넣어준다.
-    // 키가 이미 있으면 하지 않는다.
-//     stages.forEach((data,index) => {
-      
-//         if(!stageFail[data]) {
-//             // 완료한 플레이어는 제외
-//             if(data<=N){
-//                 // 클리어하지 못한 플레이어 수
-//                 const failUser = stages.filter((num) => num === data).length
-//                 // 스테이지에 도달한 플레이어 수
-//                 const tryUser = stages.filter((num) => num >= data).length
-//                 // 실패율
-//                 stageFail[data] = failUser / tryUser
-//             }
-//         }
-        
-        
-//     })
-    
+    return indexedArr
 }
