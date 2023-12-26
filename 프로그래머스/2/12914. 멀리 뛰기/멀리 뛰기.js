92317d90bc2b5464f27b44f91b663c2203d7bc4e
West_ <tjehdwn10110@naver.com>
2023-12-26 17:44:00 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.js
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.js
@@ -7,11 +7,7 @@ function solution(n) {
     let second = 1
     let cur = 0
 
-    if(n === 1) {
-        return 1 % 1234567
-    } else if (n === 2) {
-        return 2 % 1234567
-    } else {
+  
         for(let i = 1; i<=n; i++) {
             cur = (first + second) % 1234567
             
@@ -23,4 +19,3 @@ function solution(n) {
     }
     
     
-}
