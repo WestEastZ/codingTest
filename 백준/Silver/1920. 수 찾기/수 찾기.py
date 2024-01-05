1e3fbf2790315c1940c58a21b64154f716b10e4a
West_ <tjehdwn10110@naver.com>
2024-01-05 11:40:22 +0900

    [Silver IV] Title: 수 찾기, Time: 180 ms, Memory: 50380 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.py
+++ 백준/Silver/1920. 수 찾기/수 찾기.py
@@ -1,16 +1,10 @@
-N = int(input())
-arr1 = sorted(list(map(int, input().split())))
-M = int(input())
-arr2 = list(map(int, input().split()))
+n = int(input())
+n_list = set(map(int, input().split()))
+m = int(input())
+m_list = list(map(int, input().split()))
 
-dict1 = {}
-
-for i in range(len(arr1)):
-    num = arr1[i]
-    dict1[num] = num
-
-for j in arr2:
-    if dict1.get(j):
+for i in m_list:
+    if i in n_list:
         print(1)
     else:
         print(0)
