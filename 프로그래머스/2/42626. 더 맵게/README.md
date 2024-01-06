15da5fc660e0736462c483ff23dd35ef1635b09d
West_ <tjehdwn10110@naver.com>
2024-01-06 16:27:21 +0900

    [level 2] Title: 더 맵게, Time: 1664.81 ms, Memory: 51.8 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/README.md
+++ 프로그래머스/2/42626. 더 맵게/README.md
@@ -1,10 +1,10 @@
 # [level 2] 더 맵게 - 42626 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42626#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42626) 
 
 ### 성능 요약
 
-메모리: 51.8 MB, 시간: 1646.09 ms
+메모리: 51.8 MB, 시간: 1664.81 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 1월 6일 15:46:10
+2024년 1월 6일 16:27:20
 
 ### 문제 설명
 
--- 프로그래머스/2/42626. 더 맵게/더 맵게.py
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.py
@@ -4,13 +4,17 @@ def solution(scoville, K):
     heapq.heapify(scoville)
     
     while scoville[0] < K:
-        cnt += 1
+
         
         min1 = heapq.heappop(scoville)
         min2 = heapq.heappop(scoville)
         
         heapq.heappush(scoville, min1 + min2 * 2)
+        cnt += 1
         
+        # testcase 16, 22, 23  해결방법
+        # item 2개 -> 만들 수 없음
+        # 연산해도 K보다 낮음 -> 만들 수 없음       
         if len(scoville) == 2 and scoville[0] + scoville[1] * 2 < K:
             return -1
         
