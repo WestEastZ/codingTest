86cd198dc9d969c92c537a20d67b642616905802
West_ <tjehdwn10110@naver.com>
2024-01-09 11:28:56 +0900

    [Silver I] Title: 카드 합체 놀이, Time: 60 ms, Memory: 33188 KB -BaekjoonHub

---
+++ 백준/Silver/15903. 카드 합체 놀이/카드 합체 놀이.py
@@ -0,0 +1,23 @@
+import sys
+import heapq
+
+N, M = map(int, sys.stdin.readline().split())
+cards = list(map(int, sys.stdin.readline().split()))
+heapq.heapify(cards)
+answer = 0
+
+for i in range(M):
+    if len(cards) < 2:
+        break
+
+    min_card1 = heapq.heappop(cards)
+    min_card2 = heapq.heappop(cards)
+
+    sum_card = min_card1 + min_card2
+
+    for _ in range(2):
+        heapq.heappush(cards, sum_card)
+
+
+answer = sum(cards)
+print(answer)
