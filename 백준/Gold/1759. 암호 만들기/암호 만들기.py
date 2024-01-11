8599f9cfb8c90f7b59ba337d51eeeb62e5b02225
West_ <tjehdwn10110@naver.com>
2024-01-11 17:37:25 +0900

    [Gold V] Title: 암호 만들기, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/1759. 암호 만들기/암호 만들기.py
+++ 백준/Gold/1759. 암호 만들기/암호 만들기.py
@@ -6,15 +6,20 @@ from itertools import combinations
 n, m = map(int, sys.stdin.readline().split()) # 4 6
 alphabet = sorted(list(sys.stdin.readline().split())) # a t c i s w
 words = list(combinations(alphabet, n))
-aeiou = "aeiou"
+aeiou = ["a", "e", "i", "o", "u"]
 
 for word in words:
-    # 모음
-    vo = 0
-    for i in word:
-        if i in "aeiou":
-            vo += 1
+    vo = 0  # 모음
+    co = 0 # 자음
 
-    if vo >= 1 and n - vo >= 2:
+    for i in word:
+        # 모음 카운트
+        if i in aeiou:
+            vo += 1
+        # 자음 카운트
+        else:
+            co += 1
+    
+    if vo >= 1 and co >= 2:
         result = "".join(word)
         print(result)
