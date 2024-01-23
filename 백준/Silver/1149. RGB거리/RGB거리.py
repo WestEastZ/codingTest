cc1fe01d9803bfc45d081fb1f50e6cf3313a4391
West_ <tjehdwn10110@naver.com>
2024-01-23 09:49:40 +0900

    [Silver I] Title: RGB거리, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/1149. RGB거리/RGB거리.py
@@ -0,0 +1,14 @@
+import sys
+
+n = int(sys.stdin.readline())
+
+lst = []
+for _ in range(n):
+    lst.append(list(map(int, sys.stdin.readline().split())))
+
+for i in range(1, n):
+    lst[i][0] = min(lst[i - 1][1], lst[i - 1][2]) + lst[i][0]
+    lst[i][1] = min(lst[i - 1][0], lst[i - 1][2]) + lst[i][1]
+    lst[i][2] = min(lst[i - 1][0], lst[i - 1][1]) + lst[i][2]
+
+print(min(lst[n-1]))
